b6d1ca3317482b58a52db2dd63540282d0751e11
peter-keller <keller.f.peter@gmail.com>
2018-02-24 20:48:11 +0100

    feat(seconds): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/SecondsDay/SecondsDay/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SecondsDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented by the variables
+            int secondsInDay = 24 * 60 * 60;
+            int passedSeconds = (currentHours * 60 * 60) + (currentMinutes * 60) + currentSeconds;
+
+            Console.WriteLine(secondsInDay - passedSeconds);
+        }
+    }
+}
