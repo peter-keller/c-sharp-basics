8a2e32f3473029bd7d980ca2c4de067499a1863e
peter-keller <keller.f.peter@gmail.com>
2018-02-24 14:43:42 +0100

    feat(TwoNumbers): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/TwoNumbers/TwoNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 22;
+            int b = 13;
+
+            // Create a program that prints a few operations on two numbers: 22 and 13
+            // Print the result of 13 added to 22
+            Console.WriteLine(a + b);
+            // Print the result of 13 substracted from 22
+            Console.WriteLine(a - b);
+            // Print the result of 22 multiplied by 13
+            Console.WriteLine(a * b);
+            // Print the result of 22 divided by 13 (as a decimal fraction)
+            Console.WriteLine(22.0 / 13.0);
+            // Print the integer part of 22 divided by 13
+            Console.WriteLine(a / b);
+            // Print the reminder of 22 divided by 13
+            Console.WriteLine(a % b);
+        }
+    }
+}
