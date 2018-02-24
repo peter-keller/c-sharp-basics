ff98671e2379dccf072fd9157424f999fab49fd6
peter-keller <keller.f.peter@gmail.com>
2018-02-24 14:23:40 +0100

    feat(HelloOthers): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/HelloOthers/HelloOthers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greet 3 of your classmates with this program, in three separate lines
+            // like:
+            //
+            // Hello, Esther!
+            // Hello, Mary!
+            // Hello, Joe!
+
+            Console.WriteLine("Hello, Esther!");
+            Console.WriteLine("Hello, Mary!");
+            Console.WriteLine("Hello, Joe!");
+        }
+    }
+}
