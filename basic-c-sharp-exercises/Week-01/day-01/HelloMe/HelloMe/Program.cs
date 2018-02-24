bcb2d12e08fb4f3f843403e0c625d25a9624edc5
peter-keller <keller.f.peter@gmail.com>
2018-02-24 14:16:18 +0100

    feat(HelloMe): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/HelloMe/HelloMe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet you instead of the World!
+            Console.WriteLine("Hello, Me!");
+        }
+    }
+}
