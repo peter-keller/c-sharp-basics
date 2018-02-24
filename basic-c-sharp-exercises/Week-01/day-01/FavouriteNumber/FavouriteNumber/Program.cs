f878c7dd805194236ebb7f8e9e14aea1dd6d2c5a
peter-keller <keller.f.peter@gmail.com>
2018-02-24 14:58:54 +0100

    feat(FavouriteNumber): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/FavouriteNumber/FavouriteNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FavouriteNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store your favorite number in a variable (as a number)
+            // And print it like this: "My favorite number is: 8"
+            int favouriteNumber = 8;
+            Console.WriteLine("My favourite number is: " + 8);
+        }
+    }
+}
