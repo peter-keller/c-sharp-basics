493c2d6c4e62548e05d7170219425a3a8d513588
peter-keller <keller.f.peter@gmail.com>
2018-02-24 23:58:22 +0100

    feat(OddEven): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/OddEven/OddEven/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input,
+            // Then prints "Odd" if the number is odd, or "Even" if it is even.
+
+            Console.WriteLine("Please enter a number:");
+
+            int inputNumber = int.Parse(Console.ReadLine());
+            if (inputNumber % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
