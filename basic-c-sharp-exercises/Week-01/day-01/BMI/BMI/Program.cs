caaeff776384433ee025aa0d32e64d247ea7e496
peter-keller <keller.f.peter@gmail.com>
2018-02-24 19:25:53 +0100

    feat(BMI): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/BMI/BMI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the Body mass index (BMI) based on these values
+            double massInKg = 81.2;
+            double heightInM = 1.78;
+
+            Console.WriteLine(massInKg / (heightInM * heightInM));
+        }
+    }
+}
