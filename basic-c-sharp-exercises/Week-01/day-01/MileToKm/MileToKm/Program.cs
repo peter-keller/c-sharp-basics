9c8a763e3aa4d88ce49b392899474ba28c9c4859
peter-keller <keller.f.peter@gmail.com>
2018-02-24 20:55:09 +0100

    feat(kmToMiles): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/MileToKm/MileToKm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MileToKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+
+            Console.WriteLine("Please type in the amount of distance in km");
+            int kmAmount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The amount you converted to miles is: " + kmAmount * 0.62);
+
+        }
+    }
+}
