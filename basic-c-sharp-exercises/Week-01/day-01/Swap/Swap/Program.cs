7b7394eaed7e909625073578d72d63ef8a5985e9
peter-keller <keller.f.peter@gmail.com>
2018-02-24 15:09:06 +0100

    feat(Swap): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/Swap/Swap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Swap the values of the variables
+            int a = 123;
+            int b = 526;
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("a is : " + a);
+            Console.WriteLine("b is : " + b);
+        }
+    }
+}
