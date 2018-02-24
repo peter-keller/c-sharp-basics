e8df060be03ecb39200c9b41bfc8c65cdc8b810b
peter-keller <keller.f.peter@gmail.com>
2018-02-24 14:54:31 +0100

    feat(CodingHours): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/CodingHours/CodingHours/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CodingHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // An average Green Fox attendee codes 6 hours daily
+            // The semester is 17 weeks long
+            //
+            // Print how many hours is spent with coding in a semester by an attendee,
+            // if the attendee only codes on workdays.
+            //
+            // Print the percentage of the coding hours in the semester if the average
+            // work hours weekly is 52
+            int codingHours = (17 * 5) * 6;
+            double percentage = codingHours / (52.0 * 17.0);
+            Console.WriteLine(codingHours);
+            Console.WriteLine(percentage);
+        }
+    }
+}
