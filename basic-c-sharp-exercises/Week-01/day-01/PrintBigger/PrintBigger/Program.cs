a1ea23e86b9488eb9f2fb30fe3e7c4554295c48a
peter-keller <keller.f.peter@gmail.com>
2018-02-25 00:07:39 +0100

    feat(printBigger): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrintBigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+            Console.WriteLine("Enter your first number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number:");
+            int secondNUmber = int.Parse(Console.ReadLine());
+
+            if (firstNumber > secondNUmber)
+            {
+                Console.WriteLine("The first number is bigger");
+            }
+            else if (firstNumber < secondNUmber)
+            {
+                Console.WriteLine("The second number is bigger");
+            }
+            else
+            {
+                Console.WriteLine("They are equal");
+            }
+        }
+    }
+}
