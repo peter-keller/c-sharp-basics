f39c65e57c98c88bbe1a94561698956cda143ede
peter-keller <keller.f.peter@gmail.com>
2018-02-24 20:50:12 +0100

    feat(helloUser): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/HelloUser/HelloUser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + userName);
+        }
+    }
+}
