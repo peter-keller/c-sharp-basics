309fb59727c205c3b707fc7d5eb45e976cd8dc4f
peter-keller <keller.f.peter@gmail.com>
2018-02-24 14:19:39 +0100

    feat(HumptyDumpty): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/HumptyDumpty/HumptyDumpty/HumptyDumpty/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HumptyDumpty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to print Humpty Dumpty riddle correctly
+            Console.WriteLine("Humpty Dumpty sat on a wall,");
+            Console.WriteLine("Humpty Dumpty had a great fall.");
+            Console.WriteLine("All the king's horses and all the king's men");
+            Console.WriteLine("Couldn't put Humpty together again.");
+        }
+    }
+}
