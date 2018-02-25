93688349b55531f056c364b8efb7abb814a49b0c
peter-keller <keller.f.peter@gmail.com>
2018-02-25 21:26:18 +0100

    feat(appendA): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/AppendA/AppendA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `am` and assign the value `kuty` to it
+            // - Write a function called `appendA` that gets a string as an input,
+            //   appends an 'a' character to its end and returns with a string
+            // - Print the result of `appendAFunc(am)`
+
+            string am = "kuty";
+            Console.WriteLine(AppendA(am));
+
+        }
+
+        static string AppendA(string word)
+        {
+            return word + "a";
+        }
+
+    }
+}
