e90b20e083a6223940a8c8bd13e3cb449bb214de
peter-keller <keller.f.peter@gmail.com>
2018-02-25 21:58:24 +0100

    feat(PrintArguments): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/PrintArguments/PrintArguments/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PrintArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourth")
+            // ...
+
+            string[] first = { "first" };
+            string[] second = { "first", "second" };
+            string[] third = { "first", "second", "third", "fourth" };
+
+            Printer(third);
+
+        }
+
+        static void Printer(string[] word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(word[i]);
+            }
+        }
+
+    }
+}
