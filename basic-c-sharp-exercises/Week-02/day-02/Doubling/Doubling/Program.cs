18d4a5995fb050604412e234b30d4dd1eaf12a03
peter-keller <keller.f.peter@gmail.com>
2018-02-25 19:54:15 +0100

    feat(Doubling): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/Doubling/Doubling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter
+            // - Print the result of `doubling(ak)`
+
+            int ak = 123;
+            Doubling(ak);
+
+        }
+
+        static void Doubling(int number)
+        {
+            Console.WriteLine(2 * number);
+        }
+
+    }
+}
