55e074f7023207689157a1a6adb5ab7b197c4574
peter-keller <keller.f.peter@gmail.com>
2018-02-25 15:53:54 +0100

    feat(PrintEven): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/PrintEven/PrintEven/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PrintEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints all the even numbers between 0 and 500
+
+            for (int i = 0; i < 501; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
