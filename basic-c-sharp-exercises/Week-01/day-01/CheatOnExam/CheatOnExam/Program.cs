a0f467a83b26582090bc51d44c9159b9536f2d81
peter-keller <keller.f.peter@gmail.com>
2018-02-25 15:50:35 +0100

    feat(CheatOnExam): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/CheatOnExam/CheatOnExam/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CheatOnExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+
+            string message = "I won't cheat on the exam";
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
