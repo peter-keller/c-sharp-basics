070887fe53a6ba5beef957c2b6a19ec9947ff6c6
peter-keller <keller.f.peter@gmail.com>
2018-02-25 17:15:12 +0100

    feat(GuessTheNumber): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores a number, and the user has to figure it out.
+            // The user can input guesses, after each guess the program would tell one
+            // of the following:
+            //
+            // The stored number is higher
+            // The stried number is lower
+            // You found the number: 8
+
+            int number = 8;
+
+            Console.WriteLine("Guess my number");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("The stored number is lower");
+            }
+            else if (number > guess)
+            {
+                Console.WriteLine("The stored number is higher");
+            }
+            else
+            {
+                Console.WriteLine("You found the number: " + number);
+            }
+        }
+    }
+}
