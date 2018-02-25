ec301e1249d836d039fd20fcb397e97ab7c920a1
peter-keller <keller.f.peter@gmail.com>
2018-02-25 16:38:57 +0100

    feat(DrawTriangle): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DrawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+
+            Console.WriteLine("Please enter a number.");
+            int input = int.Parse(Console.ReadLine());
+            string star = "";
+            Console.WriteLine("\n");
+
+            for (int i = 1; i < input + 1; i++)
+            {
+                Console.WriteLine(star += "*");
+            }
+        }
+    }
+}
