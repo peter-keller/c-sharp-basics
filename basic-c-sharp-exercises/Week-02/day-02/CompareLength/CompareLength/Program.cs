3649ff896eaa5c29fe60a2c6ba520f652ff5e1da
peter-keller <keller.f.peter@gmail.com>
2018-02-26 00:50:44 +0100

    feat(CompareLength): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/CompareLength/CompareLength/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `p1`
+            //   with the following content: `[1, 2, 3]`
+            // - Create an array variable named `p2`
+            //   with the following content: `[4, 5]`
+            // - Print if `p2` has more elements than `p1`
+
+            int[] p1 = { 1, 2, 3 };
+            int[] p2 = { 4, 5 };
+
+            Console.WriteLine(p2.Length > p1.Length);
+
+        }
+    }
+}
