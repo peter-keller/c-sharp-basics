426ede7a5d406317ce2247c39ea4856f16ba44f6
peter-keller <keller.f.peter@gmail.com>
2018-02-25 17:11:03 +0100

    feat(DrawPyramid): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/DrawPyramid/DrawPyramid/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DrawPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+
+            Console.Write("Please enter a number: ");
+            int UserInput = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i <= UserInput; i++)
+            {
+                for (int j = 0; j < UserInput - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+    }
+}
