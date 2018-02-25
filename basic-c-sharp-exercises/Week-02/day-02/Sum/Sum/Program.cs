56276245d388a8f8e1b60d933fb92df91e5d4432
peter-keller <keller.f.peter@gmail.com>
2018-02-25 21:33:23 +0100

    feat(Summ): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/Sum/Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Write a function called `summ` that sum all the numbers
+            //   until the given parameter and returns with an integer
+
+            Console.Write("Please enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Summ(number));
+
+        }
+
+        static int Summ(int number)
+        {
+            int result = 0;
+            for (int i = 0; i < number + 1; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+    }
+}
