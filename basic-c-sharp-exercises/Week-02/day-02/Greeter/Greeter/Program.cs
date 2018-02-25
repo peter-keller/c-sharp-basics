3589fb3ecad6ce0117121a661dbb3cde08a9a7c3
peter-keller <keller.f.peter@gmail.com>
2018-02-25 21:08:35 +0100

    feat(Greeter): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/Greeter/Greeter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Greeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `al` and assign the value `Greenfox` to it
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+
+            string al = "Greenfox";
+            Console.WriteLine(Greeter(al));
+
+        }
+
+        static string Greeter(string name)
+        {
+            return "Greetings dear, " + name;
+        }
+    }
+}
