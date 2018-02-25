fda6eac719d192c9a5f2205c5517470610239d44
peter-keller <keller.f.peter@gmail.com>
2018-02-25 21:38:33 +0100

    feat(factorio): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/Factorio/Factorio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+
+            Console.Write("Please enter a number: ");
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorio(inputNumber));
+        }
+
+        static int Factorio(int number)
+        {
+            int result = 1;
+            for (int i = 1; i < number + 1; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
