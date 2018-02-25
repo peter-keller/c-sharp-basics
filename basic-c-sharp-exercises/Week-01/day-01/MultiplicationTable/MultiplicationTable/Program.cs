81eebed19bdd5a0a858245fc4dc62e8ca9f622ae
peter-keller <keller.f.peter@gmail.com>
2018-02-25 16:08:29 +0100

    feat(MultiplicationTable): exercise finished

---
+++ basic-c-sharp-exercises/Week-01/day-01/MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that asks for a number and prints the multiplication table with that number
+            //
+            // Example:
+            // The number 15 should print:
+            //
+            // 1 * 15 = 15
+            // 2 * 15 = 30
+            // 3 * 15 = 45
+            // 4 * 15 = 60
+            // 5 * 15 = 75
+            // 6 * 15 = 90
+            // 7 * 15 = 105
+            // 8 * 15 = 120
+            // 9 * 15 = 135
+            // 10 * 15 = 150
+            Console.WriteLine("Please enter a number.");
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i + " * " + inputNumber + " = " + i * inputNumber);
+            }
+        }
+    }
+}
