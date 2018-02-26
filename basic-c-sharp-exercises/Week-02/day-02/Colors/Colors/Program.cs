ca42253a69ac78b1ea4cc4e4cb4a3751c0562633
peter-keller <keller.f.peter@gmail.com>
2018-02-26 21:03:53 +0100

    feat(Colors): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/Colors/Colors/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a two dimensional array
+            //   which can contain the different shades of specified colors
+            // - In `colors[0]` store the shades of green:
+            //   `"lime", "forest green", "olive", "pale green", "spring green"`
+            // - In `colors[1]` store the shades of red:
+            //   `"orange red", "red", "tomato"`
+            // - In `colors[2]` store the shades of pink:
+            //   `"orchid", "violet", "pink", "hot pink"`
+
+
+            string[][] colors = new string[][]
+            {
+                new string[] {"lime", "forest green", "olive", "pale green", "spring green" },
+                new string[] { "orange red", "red", "tomato" },
+                new string[] { "orchid", "violet", "pink", "hot pink" }
+
+            };
+
+        }
+    }
+}
