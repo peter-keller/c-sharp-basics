f36a9229c976ce72fa0f782c6019df0cff253af3
peter-keller <keller.f.peter@gmail.com>
2018-02-26 21:14:42 +0100

    feat(AppendA): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/AppendAdvances/AppendAdvances/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendAdvances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `nimals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+
+            string[] nimals = { "kuty", "macsk", "cic" };
+
+            for (int i = 0; i < nimals.Length; i++)
+            {
+                nimals[i] += "a";
+            }
+
+            foreach(string animal in nimals)
+            {
+                Console.WriteLine(animal);
+            }
+
+        }
+    }
+}
