0b32a267f852a95f506aab5482fd4e010c35260c
peter-keller <keller.f.peter@gmail.com>
2018-02-26 16:05:02 +0100

    feat(SumElements): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/SumElements/SumElements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `r`
+            //   with the following content: `[54, 23, 66, 12]`
+            // - Print the sum of the second and the third element
+
+            int[] r = { 54, 23, 66, 12 };
+
+            Console.WriteLine(r[1] + r[2]);
+
+        }
+    }
+}
