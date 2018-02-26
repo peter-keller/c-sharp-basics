7ae908efc2fc54f43c62fe430b1c5430143938ce
peter-keller <keller.f.peter@gmail.com>
2018-02-26 20:51:56 +0100

    feat(DoubleItems): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/DoubleItems/DoubleItems/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ag`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Double all the values in the array
+
+            int[] ag = { 3, 4, 5, 6, 7 };
+            for (int i = 0; i < ag.Length; i++)
+            {
+                ag[i] *= 2;
+            }
+            
+            foreach(int number in ag)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
