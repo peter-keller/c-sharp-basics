8968542cabcac0ed2b9654a71f2d3b0c855aa788
peter-keller <keller.f.peter@gmail.com>
2018-02-26 16:24:36 +0100

    feat(IncrementElement): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/IncrementElement/IncrementElement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            //- Increment the third element
+            //- Print the third element
+
+            int[] t = { 1, 2, 3, 4, 5 };
+            t[2] += 1;
+
+            Console.WriteLine(t[2]);
+
+        }
+    }
+}
