1c6770cfe0aa8593292a7442369f58056e4003e9
peter-keller <keller.f.peter@gmail.com>
2018-02-26 16:29:22 +0100

    feat(PrintElements): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/PrintElements/PrintElements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrintElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+
+            int[] af = { 4, 5, 6, 7 };
+            foreach(int item in af)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
