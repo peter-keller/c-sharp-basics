f3188dc610f737daa1423d46bd46932b78fa9e4f
peter-keller <keller.f.peter@gmail.com>
2018-03-11 13:12:52 +0000

    feat(Reverse): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/Reverse/Reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+
+            int[] aj = { 3, 4, 5, 6, 7 };
+
+            Array.Reverse(aj);
+
+            foreach(int i in aj)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
