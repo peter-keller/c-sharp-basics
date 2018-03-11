a4584e784f5ddfd1465a2ded7d3968b2068c0eb2
peter-keller <keller.f.peter@gmail.com>
2018-03-11 12:28:01 +0000

    feat(SwapElements): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/SwapElements/SwapElements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] abc = { "first", "second", "third" };
+            string temp = abc[0];
+            abc[0] = abc[2];
+            abc[2] = temp;
+
+            foreach (string i in abc)
+            {
+                Console.WriteLine(i);
+            }
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+        }
+    }
+}
