5a899ded7f8f9327a70224e85ec7ca427a8b0485
peter-keller <keller.f.peter@gmail.com>
2018-03-11 12:32:41 +0000

    feat(SumAll): exercise finished

---
+++ basic-c-sharp-exercises/Week-02/day-02/SumAll/SumAll/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ai`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Print the sum of the elements in `ai`
+            int[] ai = { 3, 4, 5, 6, 7 };
+            int temp = 0;
+
+            foreach (int i in ai)
+            {
+                temp += i;
+            }
+            Console.WriteLine(temp);
+        }
+    }
+}
