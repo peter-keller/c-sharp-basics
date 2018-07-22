4c10b647aea474d2002346ad848e23ab92b0a8b8
peter-keller <keller.f.peter@gmail.com>
2018-07-22 18:57:25 +0200

    TakesLonger

---
+++ basic-c-sharp-exercises/Week-02/day-03/TakesLonger/TakesLonger/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TakesLonger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+            // When saving this quote a disk error has occured. Please fix it.
+            // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+            // Using pieces of the quote variable (instead of just redefining the string)
+
+            quote = quote.Insert(quote.IndexOf("you"), "always takes longer than ");
+        
+
+            Console.WriteLine(quote);
+
+            Console.ReadKey();
+        }
+    }
+}
