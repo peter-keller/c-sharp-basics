c916ced0cae16c0c74691a37493372144de0c1f7
peter-keller <keller.f.peter@gmail.com>
2018-07-23 18:12:47 +0200

    MatchMaker

---
+++ basic-c-sharp-exercises/Week-02/day-03/MatchMaker/MatchMaker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var girls = new List<string> {"Eve", "Ashley", "Bözsi", "Kat", "Jane"};
+            var boys = new List<string> {"Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"};
+
+            // Write a method that joins the two lists by matching one girl with one boy into a new list
+            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+            foreach (var item in MakingMatches(girls, boys))
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadKey();
+        }
+
+        public static List<string> MakingMatches(List<string> firstList, List<string> secondList)
+        {
+            var order = new List<string> ();
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                order.Add(firstList[i] + " - " + secondList[i]);
+                
+            }
+            return order;
+        }
+    }
+}
