16479f7df5cfc6fcec28f105c173ae8a9afe5787
peter-keller <keller.f.peter@gmail.com>
2018-07-23 17:42:46 +0200

    Reverse

---
+++ basic-c-sharp-exercises/Week-02/day-03/Reverse/Reverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a method that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+
+            Console.WriteLine(Reverse(reversed));
+            Console.ReadKey();
+        }
+
+        public static string Reverse(string word)
+        {
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
