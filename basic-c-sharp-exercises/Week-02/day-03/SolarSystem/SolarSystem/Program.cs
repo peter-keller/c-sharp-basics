75a2529b982ee6f12d5e16d3e30418a4dcb385c8
peter-keller <keller.f.peter@gmail.com>
2018-07-23 17:52:22 +0200

    SolarSystem

---
+++ basic-c-sharp-exercises/Week-02/day-03/SolarSystem/SolarSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+            planetList = PutSaturn(planetList);
+
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+            // Create a method called PutSaturn() which has list parameter and returns the correct list of planets as a string.
+
+            foreach (var item in planetList)
+            {
+                Console.Write(item + ", ");
+            }
+
+            
+            Console.ReadKey();
+            // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+        }
+
+        public static List<string> PutSaturn(List<string> planets)
+        {
+            planets.Add("Saturn");
+            return planets;
+        }
+    }
+}
