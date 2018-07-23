1c452dd63146827e5391eddf31fb2402c8e6b7c6
peter-keller <keller.f.peter@gmail.com>
2018-07-23 17:10:15 +0200

    TodoPrint

---
+++ basic-c-sharp-exercises/Week-02/day-03/TodoPrint/TodoPrint/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+
+namespace TodoPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indentation
+
+            StringBuilder correctTodoText = new StringBuilder(todoText);
+
+            correctTodoText.Insert(0, "My todo:\n")
+                           .Append(" - Download games\n     - Diablo");
+
+            
+            
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            Console.WriteLine(correctTodoText);
+
+            Console.ReadKey();
+        }
+    }
+}
